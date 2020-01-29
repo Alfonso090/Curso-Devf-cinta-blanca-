fe1b20b3660e54a3a612b2831483358d77d34820
Jose Campos <josecampostwd@gmail.com>
2020-01-28 22:04:02 -0600

    dia 6

---
+++ Dia 5/script.js
@@ -0,0 +1,56 @@
+let nombre = prompt("introduce tu nombre");
+let edad = prompt("introduce tu edad");
+let numero = 32;
+let booleano = true;
+let booleano_false = false;
+let coches =  [ "vento " ,"volcho" ];
+let nums  = [ 1,2,3,4,5]; 
+let cosas = [ 1 , "hola", true ];
+let objeto =  {firsname :"jonh" , lastname : "perez"};
+let indefinido;
+
+
+
+//estructuras de control 
+if ( nombre == "alfonso"){
+    console.log("hola alfonso¡");
+} else{
+    console.log("nombre incorrecto");
+}
+
+
+
+if (edad>18){
+    console.log("Eres mayor de edad ");
+
+}else if (edad <18){
+    console.log ("Eres menor de edad");
+}
+
+
+if (edad>= 18){
+     console.log ("eres mayor de edad ");
+
+}else if (edad < 18){
+    console.log ("Eres menor de edad");
+}else if (edad =36){
+    console.log ("estas bien viejo brother");
+} else {
+    console.log("tu edad no esta en ninguno de los rangos")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
